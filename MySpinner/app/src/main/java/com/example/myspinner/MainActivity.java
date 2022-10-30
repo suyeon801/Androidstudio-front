ee1d68e9c9d0d719760e4b169dd5f7318977d13b
suyeon <suyeon801@duksung.ac.kr>
2022-10-31 00:46:25 +0900

    spinner

---
+++ MySpinner/app/src/main/java/com/example/myspinner/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myspinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    String[] items = {"소녀시대", "걸스데이", "티아라", "여자친구"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        //ArrayAdapter -> item이 배열로 들어간다.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, items
+        );
+
+        //스피너 선택시 선택 창
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(items[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                textView.setText("선택 : ");
+            }
+        });
+    }
+}
