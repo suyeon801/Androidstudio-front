05c16a68c672bfa2737e998859a523984384dcad
suyeon <suyeon801@duksung.ac.kr>
2022-11-27 02:03:25 +0900

    MyThread

---
+++ Chapter 05/MyThread/app/src/main/java/com/example/mythread/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.mythread;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    int value = 0;
+    ValueHandler handler = new ValueHandler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        //스레드를 만들어서 생성
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //객체 생성
+                BackgroundThread thread = new BackgroundThread;
+                thread.run();
+
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("현재 값 : " +value);
+            }
+        });
+    }
+
+    //스레드 클래스 생성
+    class BackgroundThread extends Thread{
+        boolean running = false;
+
+        public void run(){
+            running = true;
+            while(running){
+                value += 1;
+
+                Message message = handler.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putInt("value", value);
+                message.setData(bundle);
+                handler.sendMessage(message);
+
+                try {
+                    Thread.sleep(1000);
+                }catch (Exception e){
+
+                }
+            }
+        }
+    }
+
+    //핸들러 클래스 생성
+    class ValueHandler extends Handler{
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            Bundle bundle = msg.getData();
+            int value = bundle.getInt("value");
+            textView.setText("현재 값 : "+value);
+
+            
+        }
+    }
+}
